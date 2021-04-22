e3438d6a9748af16bcdaa296ede0abefc2c9319b
kevin-tolentino <kevin.tolentino95@gmail.com>
2021-04-22 01:31:29 -0700

    added word dictionary in array form, and also started implementing trie package in function

--- vanity.js
+++ vanity.js
@@ -1,9 +1,18 @@
-const wordArray = require('./10000-common-word.js')
+// const wordArray = require('./10000-common-word')
+const wordsArray = require('./wordsArray')
+const TrieSearch = require('trie-search/src/TrieSearch');
 
-// console.log(wordArray)
+
+console.log(Array.isArray(wordsArray.wordsArray), 'print')
 
 const findMeSomeVanityNumbers = () => {
+  const ts = new TrieSearch('word')
+  ts.addAll(wordsArray.wordsArray)
+  const result = ts.get('sign', null, 1)
+  console.log(result)
   return true
 }
+
+findMeSomeVanityNumbers()
 
 module.exports = findMeSomeVanityNumbers
