5fd59037f167f39b0b0ebd0fada2af75eced1fc9
kevin-tolentino <kevin.tolentino95@gmail.com>
2021-04-22 22:37:17 -0700

    succeeded in first pass of vanity function

--- vanity.js
+++ vanity.js
@@ -26,7 +26,7 @@ const returnNumberPossibilities = (numberFromCaller) => {
   return [targetDigitsOne, targetDigitsTwo];
 };
 
-//change the numbers to letter sets according to the digits
+//change the numbers to letter sets according to the digits and puts them in an object
 const numberToLetterSets = (digitStringArray) => {
   const letterSetsObj = {};
   const letterSetsArrayOne = [];
@@ -52,51 +52,55 @@ const numberToLetterSets = (digitStringArray) => {
   return letterSetsObj;
 };
 
-const getCombinations = (arr) => {
+//find all possible string combinations with first index as base for string
+const getStringCombinations = (arr) => {
   if (arr.length === 1) {
     return arr[0];
   }
-  const ans = [];
+  const combinationArray = [];
 
-  const otherCases = getCombinations(arr.slice(1));
+  const otherCases = getStringCombinations(arr.slice(1));
   for (let i = 0; i < otherCases.length; i++) {
     for (let j = 0; j < arr[0].length; j++) {
-      ans.push(arr[0][j] + otherCases[i]);
+      combinationArray.push(arr[0][j] + otherCases[i]);
     }
   }
-  return ans;
+  return combinationArray;
 };
 
-const setToStringPossibilities = (letterSetsArray) => {
-  const stringPossibilities = [];
-};
-
-// function to loop through array and call the trie search to retrieve words
-// const generateLetterPossibilities = (number) => {
-//   arrayOfScrambledStrings = [];
-//   // go through string indexes
-//   for (let x = 0; x < number.length; x++) {
-//     return true;
-//   }
-//   if (string === '1') {
-//
-//   }
-//
-//   return arrayOfScrambledStrings;
-// };
-
-const findMeSomeVanityNumbers = () => {
+const findMeSomeVanityNumbers = (stringPossibilitiesObject) => {
   const ts = new TrieSearch('word');
   ts.addAll(dictionaryArray.wordsArray);
-  const result = ts.get('pig', null, 1);
-  console.log(result);
-  return true;
+
+  const possibilitiesArray = [];
+  const arrayOne = getStringCombinations(stringPossibilitiesObject.first);
+  const arrayTwo = getStringCombinations(stringPossibilitiesObject.second);
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    const result = ts.get(arrayOne[i], null, 1);
+    if (result.length !== 0) {
+      possibilitiesArray.push(result[0].word);
+    }
+  }
+
+  for (let i = 0; i < arrayTwo.length; i++) {
+    const result = ts.get(arrayTwo[i], null, 1);
+    if (result.length !== 0) {
+      possibilitiesArray.push(result[0].word);
+    }
+  }
+
+  console.log(possibilitiesArray);
+  return possibilitiesArray;
 };
 
-// findMeSomeVanityNumbers();
+findMeSomeVanityNumbers({
+  first: ['pqrs', 'ghi', 'ghi', 'mno'],
+  second: ['ghi', 'jkl', 'mno'],
+});
 
 // returnNumberPossibilities()
 
-numberToLetterSets(['7546', '456']);
+// numberToLetterSets(['7546', '456']);
 
 module.exports = findMeSomeVanityNumbers;
