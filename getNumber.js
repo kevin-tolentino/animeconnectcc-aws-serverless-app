bc0316749fc0d11e70106b810f9864de67efc6dd
Kevin Tolentino <kevin.tolentino95@gmail.com>
2021-04-25 22:42:23 -0700

    Moved functions and code related to get and post in their respective places. Got sample data successfully from ddb using lamda console testing.

--- getNumber.js
+++ getNumber.js
@@ -1,19 +1,36 @@
-/* eslint-disable */
-const findMeSomeVanityNumbers  = require('./vanity.js');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const table = 'animeconnectcc-aws-serverless-app-dev';
 
 module.exports.getNumber = function (event, context, callback) {
-  // const testVanity = findMeSomeVanityNumbers({
-  //   first: ['pqrs', 'ghi', 'ghi', 'mno'],
-  //   second: ['ghi', 'jkl', 'mno'],
-  // });
-  let receivedCallerSubmittedNumber = event['Details']['ContactData']['CustomerEndpoint']['Address']
+  // eslint-disable-next-line dot-notation
+  const receivedCallerSubmittedNumber = event['Details']['ContactData']['CustomerEndpoint']['Address'];
 
-  const resultMap = {
-    sentLambdaGetNumber: receivedCallerSubmittedNumber,
+  const params = {
+    TableName: table,
+    Key: {
+      number: receivedCallerSubmittedNumber,
+    },
   };
-  // console.log('test vanity', testVanity)
-  console.log('receivedCallerSubmittedNumberssss', receivedCallerSubmittedNumber)
-  // console.log('resultMap', resultMap)
 
-  callback(null, resultMap);
+  dynamoDb.get(params, (err, data) => {
+    if (err) {
+      console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2));
+    } else {
+      console.log('DATA GET TEST', data.Item);
+      console.log('DATA specifics', data.Item.info.two, typeof data.Item.info.two);
+      const returnResponse = {
+        statusCode: 200,
+        body: JSON.stringify(data.Item),
+      };
+      callback(null, returnResponse);
+    }
+  });
+
+  // const resultMap = {
+  //   sentLambdaGetNumber: receivedCallerSubmittedNumber,
+  // };
+
+  // callback(null, null);
 };
--- postNumber.js
+++ postNumber.js
@@ -1,23 +1,30 @@
 const AWS = require('aws-sdk');
 
-// AWS.config.setPromisesDependency(require('bluebird'));
-
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+//const findMeSomeVanityNumbers = require('./vanity.js');
 
 // eslint-disable-next-line no-unused-vars
 module.exports.submit = (event, context, callback) => {
+  // eslint-disable-next-line dot-notation
+  const receivedCallerSubmittedNumber = event['Details']['ContactData']['CustomerEndpoint']['Address'];
   const table = 'animeconnectcc-aws-serverless-app-dev';
-  const number = '+15623037456';
   const samplePostItem = {
-    number,
+    receivedCallerSubmittedNumber,
     info: {
-      one: 'sign',
-      two: 'shin',
-      three: 'gln',
-      four: 'glow',
-      five: 'ilo',
+      1: 'sign',
+      2: 'shin',
+      3: 'gln',
+      4: 'glow',
+      5: 'ilo',
     },
   };
+
+  const resultMap = {
+    sentLambdaGetNumber: receivedCallerSubmittedNumber,
+  };
+  // console.log('test vanity', testVanity)
+  console.log('receivedCallerSubmittedNumber', receivedCallerSubmittedNumber);
+  // console.log('resultMap', resultMap)
 
   const params = {
     TableName: table,
@@ -32,4 +39,6 @@ module.exports.submit = (event, context, callback) => {
       console.log('Added item:', JSON.stringify(data, null, 2));
     }
   });
+
+  callback(null, resultMap);
 };
